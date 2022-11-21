01cf39002d4ab58649861f02a9c7e7d703e25694
subhankar29 <subhankar29shah@gmail.com>
2022-11-20 23:32:32 -0500

    A8 assignment commit.

--- src/services/tuits-thunks.js
+++ src/services/tuits-thunks.js
@@ -22,7 +22,7 @@ export const findAllTuits = async (dispatch) => {
 }
 
 export const deleteTuit = async (dispatch, tuit) => {
-    const response = await service.deleteTuit(tuit);
+    await service.deleteTuit(tuit);
     dispatch({
         type: DELETE_TUIT,
         tuit
@@ -30,7 +30,7 @@ export const deleteTuit = async (dispatch, tuit) => {
 }
 
 export const updateTuit = async (dispatch, tuit) => {
-    const status = await service.updateTuit(tuit);
+    await service.updateTuit(tuit);
     dispatch({
         type: UPDATE_TUIT,
         tuit
